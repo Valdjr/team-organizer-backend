f940992ad58b2583e3c4be5e38f99b24a242c54a
Valdir Júnior <31621712+Valdjr@users.noreply.github.com>
2019-09-16 08:35:11 -0300

    Disable x-powered-by

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.disable('x-powered-by');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
