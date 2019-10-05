1eb36fa10bbb565a4643cd35d8e941ccabce4a4d
Paulo <pauloantoniolfc@gmail.com>
2019-10-05 20:34:10 -0300

    correção do cores para aceitar a o method delete

--- src/index.js
+++ src/index.js
@@ -16,9 +16,10 @@ const criarUsers = require("./routes/criarUsers");
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 });
